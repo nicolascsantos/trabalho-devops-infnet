af45008bc26197d93a009182087955da241befa7
Nicolas de Carvalho <nicolas.carvalho@hotmail.com.br>
2026-04-06 22:32:01 -0300

    global exception filter

--- src/TrabalhoDevOpsInfnet.API/Filters/APIGlobalExceptionFilter.cs
+++ src/TrabalhoDevOpsInfnet.API/Filters/APIGlobalExceptionFilter.cs
@@ -1,6 +1,56 @@
-﻿namespace TrabalhoDevOpsInfnet.API.Filters
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TrabalhoDevOpsInfnet.Domain.Exceptions;
+
+namespace TrabalhoDevOpsInfnet.API.Filters
 {
-    public class APIGlobalExceptionFilter
+    public class APIGlobalExceptionFilter : IExceptionFilter
     {
+        private readonly IHostEnvironment _env;
+
+        public APIGlobalExceptionFilter(IHostEnvironment env)
+            => _env = env;
+
+        public void OnException(ExceptionContext context)
+        {
+            var details = new ProblemDetails();
+            var exception = context.Exception;
+            if (_env.IsDevelopment())
+                details.Extensions.Add("StackTrace", exception.StackTrace);
+            if (exception is EntityValidationException)
+            {
+                var ex = exception as EntityValidationException;
+                details.Title = "One or more validation errors occurred.";
+                details.Status = StatusCodes.Status422UnprocessableEntity;
+                details.Detail = ex!.Message;
+                details.Type = "UnprocessableEntity";
+            }
+            else if (exception is NotFoundException)
+            {
+                var ex = exception as NotFoundException;
+                details.Title = "One or more validation errors occurred.";
+                details.Status = StatusCodes.Status404NotFound;
+                details.Detail = ex!.Message;
+                details.Type = "NotFound";
+            }
+            else if (exception is RelatedAggregateException)
+            {
+                var ex = exception as RelatedAggregateException;
+                details.Title = "Invalid Related Aggregate.";
+                details.Status = StatusCodes.Status422UnprocessableEntity;
+                details.Detail = ex!.Message;
+                details.Type = "RelatedAggregate";
+            }
+            else
+            {
+                details.Title = "An unexpected error occured.";
+                details.Status = StatusCodes.Status422UnprocessableEntity;
+                details.Type = "UnexpectedError";
+                details.Detail = exception.Message;
+            }
+            context.HttpContext.Response.StatusCode = (int)details.Status!;
+            context.Result = new ObjectResult(details);
+            context.ExceptionHandled = true;
+        }
     }
 }
